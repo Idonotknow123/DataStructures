76dd9e8d6a657e8ce057fbb71beffa9b926bb500
Idonotknow123 <ireneuszszuba74@gmail.com>
2025-12-31 21:21:28 +0100

    adding template file

---
+++ TEMPLATE/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int i = 0;
+    while(i< 5) {
+        std::cout << "Count: " << i << std::endl;
+        i++;
+    }
+    return 0;
+}
